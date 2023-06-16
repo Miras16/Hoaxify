c3bcd4c2ca13ec66c00466cb486f0baa073b228f
Miras <mirastilegenov518@gmail.com>
2023-06-16 14:47:47 +0600

    signup success routing

--- frontend/src/containers/App.spec.js
+++ frontend/src/containers/App.spec.js
@@ -1,37 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import UserSignupPage from '../pages/UserSignupPage';
+import UserPage from '../pages/UserPage';
+import * as apiCalls from '../api/apiCalls';
 
-const setup = (path) => {
-  return render(
-    <MemoryRouter initialEntries={[path]}>
-      <App />
-    </MemoryRouter>
-  );
+const actions = {
+  postLogin: apiCalls.login,
+  postSignup: apiCalls.signup
 };
 
-describe('App', () => {
-  it('displays homepage when url is /', () => {
-    const { queryByTestId } = setup('/');
-    expect(queryByTestId('homepage')).toBeInTheDocument();
-  });
-  it('displays LoginPage when url is /login', () => {
-    const { container } = setup('/login');
-    const header = container.querySelector('h1');
-    expect(header).toHaveTextContent('Login');
-  });
-  it('displays only LoginPage when url is /login', () => {
-    const { queryByTestId } = setup('/login');
-    expect(queryByTestId('homepage')).not.toBeInTheDocument();
-  });
-  it('displays UserSignupPage when url is /signup', () => {
-    const { container } = setup('/signup');
-    const header = container.querySelector('h1');
-    expect(header).toHaveTextContent('Sign Up');
-  });
-  it('displays userpage when url is other than /, /login or /signup', () => {
-    const { queryByTestId } = setup('/user1');
-    expect(queryByTestId('userpage')).toBeInTheDocument();
-  });
-});
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route
+            path="/login"
+            component={(props) => <LoginPage {...props} actions={actions} />}
+          />
+          <Route
+            path="/signup"
+            component={(props) => (
+              <UserSignupPage {...props} actions={actions} />
+            )}
+          />
+          <Route path="/:username" component={UserPage} />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
